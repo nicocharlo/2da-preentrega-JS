ec2639d45707e2458eedb964c4f852850fc07de5
Nicolas DEV <nicolascharlo@hotmail.com>
2024-08-14 00:17:53 -0300

    Segunda preentrega.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+/* Objetivos específicos
+
+Capturar entradas mediante prompt().
+Declarar variables y objetos necesarios para simular el proceso seleccionado.
+Crear funciones y/o métodos para realizar operaciones (suma, resta, concatenación, división, porcentaje, etc).
+Efectuar una salida, que es el resultado de los datos procesados, la cual puede hacerse por alert() o console.log().
+ */
+
